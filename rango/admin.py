c0e1b7142043497d6ddc01bc96d61d92d9bd31af
Nicholas Cheong <nicholas_cheong141@outlook.com>
2021-07-28 15:56:19 +0200

    Changed the admin page to now display which category and url each page belongs to in the Page section

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+# Create model admin class to pass to register. 
+# This changes he admin options for the app
+class pageAdmin(admin.ModelAdmin):
+
+    # List display admin option (tuple of field names to display on the list page for the object)
+    list_display = ('title','category','url')
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, pageAdmin)
