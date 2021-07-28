3b8f0d05499ff5d762965e1e7f784d4648128177
Nicholas Cheong <nicholas_cheong141@outlook.com>
2021-07-28 15:30:27 +0200

    Changed the population script to now include views and likes for categories

--- populate_rango.py
+++ populate_rango.py
@@ -33,17 +33,19 @@ def populate():
         'url':'http://flask.pocoo.org'} ]
     
     # Create dictionary of dictionaries (the catagories)
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages, 'views': 128,'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64,'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32,'likes': 16} }
 
 
     # Adding all catagory pages to DB
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+
+        # cat_data is dictionary, cat is the key (or name of the category in this case.)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
+        
         for p in cat_data['pages']:
             add_page(c,p['title'], p['url'])
-
 
     # Print out the catagories
     for c in Category.objects.all():
@@ -58,8 +60,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
