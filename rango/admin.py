2c255bfbe189a0f345fc3dcda29253164f10dee1
Nicholas Cheong <nicholas_cheong141@outlook.com>
2021-07-28 18:39:23 +0200

    Added slug variable to the Category model and the functionality to autogenerate it when a name is typed

--- rango/admin.py
+++ rango/admin.py
@@ -3,11 +3,14 @@ from rango.models import Category, Page
 
 # Create model admin class to pass to register. 
 # This changes he admin options for the app
-class pageAdmin(admin.ModelAdmin):
+class PageAdmin(admin.ModelAdmin):
 
     # List display admin option (tuple of field names to display on the list page for the object)
     list_display = ('title','category','url')
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Page, pageAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
