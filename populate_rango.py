571486d7189e2bbed7e9f52640d556227ab20a99
Nicholas Cheong <nicholas_cheong141@outlook.com>
2021-07-28 19:57:51 +0200

    Changed rango to now display 5 most viewed webpages on index page. Population script now adds viewes to pages too

--- populate_rango.py
+++ populate_rango.py
@@ -12,25 +12,33 @@ def populate():
 
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/'},
+         'url':'http://docs.python.org/3/tutorial/',
+         'views': 80},
         {'title':'How to Think like a Computer Scientist',
-        'url':'http://www.greenteapress.com/thinkpython/'},
+        'url':'http://www.greenteapress.com/thinkpython/',
+         'views': 91},
         {'title':'Learn Python in 10 Minutes',
-        'url':'http://www.korokithakis.net/tutorials/python/'} ]
+        'url':'http://www.korokithakis.net/tutorials/python/',
+         'views': 95} ]
     
     django_pages = [
         {'title':'Official Django Tutorial',
-        'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+        'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+         'views': 100},
         {'title':'Django Rocks',
-        'url':'http://www.djangorocks.com/'},
+        'url':'http://www.djangorocks.com/',
+         'views': 9},
         {'title':'How to Tango with Django',
-        'url':'http://www.tangowithdjango.com/'} ]
+        'url':'http://www.tangowithdjango.com/',
+         'views': 23} ]
 
     other_pages = [
         {'title':'Bottle',
-        'url':'http://bottlepy.org/docs/dev/'},
+        'url':'http://bottlepy.org/docs/dev/',
+         'views': 79},
         {'title':'Flask',
-        'url':'http://flask.pocoo.org'} ]
+        'url':'http://flask.pocoo.org',
+         'views': 13} ]
     
     # Create dictionary of dictionaries (the catagories)
     cats = {'Python': {'pages': python_pages, 'views': 128,'likes': 64},
@@ -45,7 +53,7 @@ def populate():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
         
         for p in cat_data['pages']:
-            add_page(c,p['title'], p['url'])
+            add_page(c,p['title'], p['url'], p['views'])
 
     # Print out the catagories
     for c in Category.objects.all():
--- rango/views.py
+++ rango/views.py
@@ -13,11 +13,12 @@ def index(request):
     # Place the list in our context_dict dictionary (with our boldmessage!)
     # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list_views = Page.objects.order_by('-views')[:5]
 
-    print(category_list)
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['mostViewedPages'] = page_list_views
 
     print(context_dict)
     # Return a rendered response to send to the client.
