978f1d953bda8aece3de7659318522c337970f58
Nicholas Cheong <nicholas_cheong141@outlook.com>
2021-07-29 17:37:00 +0200

    Added custom template tags to output category lists. Adjusted html templates accordingly

--- rango/views.py
+++ rango/views.py
@@ -31,14 +31,14 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # prints out whether the method is a GET or a POST
+    print(request.method)
+
+    # prints out the user name, if no one is logged in it prints `AnonymousUser`
+    print(request.user)
     
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/about.html', context=context_dict)
+    # {} since render requires a dictionary parameter, passing an empty dictionary
+    return render(request, 'rango/about.html', {})
 
 # Show the category when selected 
 def show_category(request, category_name_slug):
