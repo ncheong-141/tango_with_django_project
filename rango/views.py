9954c0e417dc24f845bc85178cc2c3e48b38dd01
Nicholas Cheong <nicholas_cheong141@outlook.com>
2021-07-30 12:22:52 +0200

    Changed add category and page functionality to only be possible by users. Changed restricted page to use a template which inherits from the base template

--- rango/views.py
+++ rango/views.py
@@ -10,7 +10,7 @@ from rango.forms import CategoryForm
 from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.urls import reverse
@@ -78,7 +78,7 @@ def show_category(request, category_name_slug):
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
 
-
+@login_required
 def add_category(request):
     
     form = CategoryForm()
@@ -103,7 +103,7 @@ def add_category(request):
     # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
 
-
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -224,4 +224,12 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
+
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+   
+    # Take the user back to the homepage.
+    return redirect(reverse('rango:index'))
